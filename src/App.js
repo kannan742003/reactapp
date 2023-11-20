696b25ff8a34cbd753b28c96fba5bcb6dec25024
kannan742003 <kannan742003@gmail.com>
2023-11-20 16:36:38 +0530

    app.js and app.css updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+// src/App.js
 import React from 'react';
 import './App.css';
+
+// Additional Component
+const Greeting = ({ name }) => (
+  <div className="greeting">
+    <h2>Welcome to, {name}!</h2>
+    <p>This is our React app.</p>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React! kannan</h1>
+      <h1>Hello, All! </h1>
+      <Greeting name="Abhyaz" />
     </div>
   );
 }
