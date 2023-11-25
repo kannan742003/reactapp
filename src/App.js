0e4e0f7c880af72f55e4c562a9ae6401fc4f32a8
Ubuntu <ubuntu@ip-172-31-28-205.ec2.internal>
2023-11-25 13:27:18 +0000

    App.js greeting name is changed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, EveryOne! </h1>
-      <Greeting name="Abhyaz MTAB Technology" />
+      <Greeting name="Abhyaz" />
     </div>
   );
 }
